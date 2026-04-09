71a0f3bfc74195af55c4c1c67ce3c549dbbc7235
Iban Cervera <Ibancoder@gmail.com>
2026-04-09 07:53:09 +0200

    Application.yaml and @getters and @setters added

--- src/main/java/com/teletecnics/invoices_back/model/Client.java
+++ src/main/java/com/teletecnics/invoices_back/model/Client.java
@@ -2,16 +2,13 @@ package com.teletecnics.invoices_back.model;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name="clientes")
-@Data //Genera getters, setters, toString, equals y hashCode con Lombok
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Client {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,5 +25,74 @@ public class Client {
     private String email;
     private String telefono;
 
+    //Constructor
+    public Client(){
+
+    }
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 
 }
--- src/main/java/com/teletecnics/invoices_back/model/Invoice.java
+++ src/main/java/com/teletecnics/invoices_back/model/Invoice.java
@@ -3,16 +3,11 @@ package com.teletecnics.invoices_back.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "invoices")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+
 
 
 public class Invoice {
@@ -33,6 +28,109 @@ public class Invoice {
     private Integer tipoIva; //0 o 21
     private Double importeIva;
     private Double totalFactura;
+
+
+    //Constructor
+    public Invoice(){
+
+    }
+    public Boolean getCobrada() {
+        return cobrada;
+    }
+
+    //Getters and Setters
+    public void setCobrada(Boolean cobrada) {
+        this.cobrada = cobrada;
+    }
+
+    public LocalDate getFechaPrevistaCobro() {
+        return fechaPrevistaCobro;
+    }
+
+    public void setFechaPrevistaCobro(LocalDate fechaPrevistaCobro) {
+        this.fechaPrevistaCobro = fechaPrevistaCobro;
+    }
+
+    public Double getTotalFactura() {
+        return totalFactura;
+    }
+
+    public void setTotalFactura(Double totalFactura) {
+        this.totalFactura = totalFactura;
+    }
+
+    public Double getImporteIva() {
+        return importeIva;
+    }
+
+    public void setImporteIva(Double importeIva) {
+        this.importeIva = importeIva;
+    }
+
+    public Integer getTipoIva() {
+        return tipoIva;
+    }
+
+    public void setTipoIva(Integer tipoIva) {
+        this.tipoIva = tipoIva;
+    }
+
+    public Double getBaseImponible() {
+        return baseImponible;
+    }
+
+    public void setBaseImponible(Double baseImponible) {
+        this.baseImponible = baseImponible;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Client getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Client cliente) {
+        this.cliente = cliente;
+    }
+
+    public Long getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(Long clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public LocalDate getFechaFactura() {
+        return fechaFactura;
+    }
+
+    public void setFechaFactura(LocalDate fechaFactura) {
+        this.fechaFactura = fechaFactura;
+    }
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     private LocalDate fechaPrevistaCobro;
     private Boolean cobrada;
 }
